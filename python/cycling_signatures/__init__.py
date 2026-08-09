f4308b43d829105db39b16c844e55ea1c31ab712
Travis Casey <travis.w.casey@outlook.com>
2026-08-09 12:15:13 -0500

    Add license header.

--- python/cycling_signatures/__init__.py
+++ python/cycling_signatures/__init__.py
@@ -1,3 +1,6 @@
+# This file is part of cycling-signatures, licensed under the GPL-3.0-or-later.
+# See LICENSE or <https://www.gnu.org/licenses/gpl-3.0.html>.
+
 """Algebraic topological descriptions of recurrent motions in high-dimensional
 dynamical systems.
 
